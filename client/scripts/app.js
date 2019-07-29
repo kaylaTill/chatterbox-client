4ea796ed9ce5c4b9e13b62b005f75de66e4dd574
kaylatillman <kaylatillman319@gmail.com>
2019-07-29 08:23:11 -0700

    Messages rendered to screen/ get submit button to work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,35 +3,35 @@ var App = {
   $spinner: $('.spinner img'),
 
   username: 'anonymous',
+  data: 'none',
 
-  initialize: function() {
+
+  initialize: function () {
     App.username = window.location.search.substr(10);
 
-    FormView.initialize();
     RoomsView.initialize();
-    MessagesView.initialize();
 
     // Fetch initial batch of messages
     App.startSpinner();
     App.fetch(App.stopSpinner);
-
   },
 
-  fetch: function(callback = ()=>{}) {
+  fetch: function (callback = () => { }) {
     Parse.readAll((data) => {
       // examine the response from the server request:
       console.log(data);
-
+      App.data = data;
+      MessagesView.initialize();
       callback();
     });
   },
 
-  startSpinner: function() {
+  startSpinner: function () {
     App.$spinner.show();
     FormView.setStatus(true);
   },
 
-  stopSpinner: function() {
+  stopSpinner: function () {
     App.$spinner.fadeOut('fast');
     FormView.setStatus(false);
   }
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -2,10 +2,24 @@ var MessagesView = {
 
   $chats: $('#chats'),
 
-  initialize: function() {
+  initialize: function () {
+    MessagesView.$chats.append(MessagesView.render)
+
   },
 
-  render: function() {
+  render: function () {
+    var html = '';
+    for (var i = 0; i < App.data.results.length; i++) {
+      if (App.data.results[i].username === undefined) {
+        App.data.results[i].username = 'troller'
+      }
+      if (App.data.results[i].text === undefined) {
+        App.data.results[i].text = 'troller-pt2'
+      }
+      html += MessageView.render(App.data.results[i]);
+    }
+    return html;
   }
+
 
 };
